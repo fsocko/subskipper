1a19aa1b4fe58c20d5f1d6c09c55a835cc35c69f
fps <fps@gmail.com>
2015-07-25 16:58:42 +0100

    Should be clean and have project now

---
+++ src/coreLogic/MethodTest.java
@@ -0,0 +1,23 @@
+package coreLogic;
+
+public class MethodTest {
+
+	public static void main(String[] args) 
+	{
+		OKane test1 = new OKane();
+		//constructs a torpedo. INPUTS: name, slow/fast speed (kn), rangeS, rangeF (m)
+		Torpedo mk10 = new Torpedo("Mk. 10", 36, -1, 3200, -1);
+		Torpedo mk14 = new Torpedo("Mk. 14", 31, 46, 8200, 4100);
+		Torpedo mk16 = new Torpedo("Mk. 16", 46, -1, 12500, -1);
+		
+		int AOB = 0;
+		double targS = 6;
+		char selSpeed = 'f';
+		Torpedo fireTorp = mk14; 
+		//oKSolution(int AOB, double targS, char selSpeed, Torpedo fireTorp)
+		System.out.println("AOB: " + AOB + " targSpeed: " + targS +" TorpSpeed: " +selSpeed + fireTorp.getName());
+		System.out.println("Scope Bearing: " + test1.oKSolution(AOB, targS, selSpeed, fireTorp));
+	}
+	
+
+}
---
+++ src/coreLogic/OKane.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package coreLogic;
+
+import java.lang.Math;
+/**
+ * @author fps
+ *
+ */
+//Lead Angle for O'Kane firing solution. - note that method does
+//not know whether to add or subtract ie if T is facing port or
+//stbd
+//Inputs: Torpedo Speed (kn), target Speed (kn)
+public class OKane {
+	
+	
+	//TODO: check range to target is not too large for the solution
+	//Method for generating OKane firing solution, returns firing bearing.
+	//Inputs: AOB- determines port or strbd, Speed (F/S) default to slow; torpedo object
+	public double oKSolution(int AOB, double targS, char selSpeed, Torpedo fireTorp)
+	{
+		double solBearing = -1;
+		boolean stbd = true;
+		//check if the position is correct. Sub needs to be in front of target, on either
+		//Stbd or port side. this means AOB is either 0-90 for stbd or  270-360 for port
+		//if anything else happens, solution is invalid, and we return a flag.
+		boolean invalidSol = false;
+		if(AOB <= 90 && AOB<360){
+			stbd = true;
+		}
+		else if(AOB>=270 && AOB<360 ){
+			stbd = false;
+		}
+		else if(AOB == 0){
+			invalidSol = true; //for now let's assume the user is an idiot if AOB = 0
+		}
+		else{invalidSol = true;}
+		
+		//Input correct speed to solution based on user input
+		double torpFireS = 0; //torpSpeed in knots
+		switch (selSpeed){
+		//TODO: what if there's no fast setting?
+			case 'f':{
+				torpFireS = fireTorp.getSpeedF(); //fast setting
+				if (torpFireS < 0){
+					torpFireS = fireTorp.getSpeedS();
+					System.out.println("No Fast Speed available.");
+					//if there is no fast speed, set it to slow: the user is an idiot.
+				}
+			}
+			
+			break;
+			case 's': torpFireS = fireTorp.getSpeedS(); //slow setting
+			break;
+		}
+		
+		if(invalidSol == true){
+			solBearing = -1;
+		}
+		//if stbd, subtract from 360.
+		else if(stbd == true){
+			solBearing = 360-okaneLead(torpFireS, targS);
+		}
+		//If port, add to 0 for lead bearing.
+		else if(stbd == false){
+			solBearing = 0 + okaneLead(torpFireS, targS);
+		}
+		return solBearing; //tSpeed into TDC, set your scope to this bearing, fire
+		}
+	
+	//Input: torpedo speed (kn), target speed (kn)
+	public double okaneLead(double torpS, double targS){
+		if(torpS < 1 || targS <1){
+			return -1;}
+		double lead = 0;
+		//90 - inverseTan(torpS/targS)
+		double rad = (torpS/targS);
+		lead =  90-Math.toDegrees(Math.atan(torpS/targS));
+		if(lead > 90){
+			lead = -1;} //Okane relies on being ahead of the target,
+						//we would be aiming backwards.
+		return lead;
+	}
+}
