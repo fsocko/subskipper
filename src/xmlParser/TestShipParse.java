93293f0ee90f3ea85850a872a9280c9035cb4770
fps <fps@gmail.com>
2015-08-05 15:30:31 +0100

    Tested with other records.

--- src/xmlParser/TestShipParse.java
+++ src/xmlParser/TestShipParse.java
@@ -6,9 +6,11 @@ public class TestShipParse {
 
 		//readShips from file:
 		ReadShips readTest = new ReadShips();
-		readTest.readShipRecord("shipData/Sea/NCA_Mogami/NCA_Mogami.cfg");
+		readTest.readShipRecord("shipData/Sea/COKaibokan2/COKaibokan2.cfg");
+		readTest.printTempShips();
 		Ship USSEnterprise = readTest.makeShip();
 		System.out.println(USSEnterprise.toString());
+
 		
 	}
 
