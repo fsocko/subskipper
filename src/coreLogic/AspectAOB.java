51c765d40a95b2628cac6b7cb7235c0660242b34
fps <fps@gmail.com>
2015-08-14 16:52:56 +0100

    Start of AspectAOB method

---
+++ src/coreLogic/AspectAOB.java
@@ -0,0 +1,8 @@
+package coreLogic;
+
+//Method for determining the AOB based on an observed and reference aspect ratio.
+//Note, this method requires an approximate visual AOB or it will return wildly
+//inaccurate figures.
+public class AspectAOB {
+
+}
