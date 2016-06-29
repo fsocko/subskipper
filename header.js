909c33bff29ca14a2c849746711cc8fc86350e64
fsocko <fsocko@gmail.com>
2016-06-29 18:30:37 +0100

    Made some screw and steel textures, impemented screws in panels, needs a background that's open as well as smaller screws, but fairly happy for now.

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.write('\
 <header>\
-    <div id=headerDiv>\
+    <div class="header screwpanel" id=headerDiv>\
       <a href="https://fsocko.github.io/SubSkipper/" class="btn"><h1>SubSkipper</h1></a>\
 	  <h2>A series of tools for plotting torpedo solutions.</h2>\
 	  <a href="https://github.com/fsocko/SubSkipper" class="btn">View on GitHub</a> <br />\
