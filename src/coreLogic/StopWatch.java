38deb77bb01138b6b310e0f99620ff5c62521b07
fsocko <fsocko@gmail.com>
2016-08-23 21:00:29 +0100

    method stubs for modular stopwatch

---
+++ src/coreLogic/StopWatch.java
@@ -0,0 +1,20 @@
+package coreLogic;
+
+public class StopWatch {
+
+	
+	//Given known ship length, find speed in ms.
+	public void fixedWire(){}
+	
+	//Given known range and Torpedo Speed, find time. 
+	//(TODO: calculate speed per gyro angle)
+	public void timeToImpact(){}
+	
+	//Given known speed, and time variable, calculate distance travelled, in m.
+	public void SpeedToDistance(){}
+	
+	public void timerStart(){}
+	
+	public void timerStop(){}
+	
+}
