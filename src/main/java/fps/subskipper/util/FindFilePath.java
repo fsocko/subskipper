1734e361969a7149649a3a0d26250536484794a9
fsocko <fsocko@google.com>
2018-10-07 19:10:19 +0100

    Restored workspace, made findFile compatible with directory search.

--- src/main/java/fps/subskipper/util/FindFilePath.java
+++ src/main/java/fps/subskipper/util/FindFilePath.java
@@ -6,20 +6,19 @@ public class FindFilePath {
 
     public File found;
 
-    public String findFilepathRecursively (String startDirectory, String filename){
-        return findFilepathRecursively(new File[]{new File(startDirectory)}, filename);
+    public String findFile(String startDirectory, String filename){
+        return findFile(new File[]{new File(startDirectory)}, filename);
     }
 
-    public String findFilepathRecursively (File[]startDirectory, String filename){
+    public String findFile(File[] startDirectory, String filename){
         for (File file : startDirectory) {
-            if (file.isFile() && file.getName().contains(filename)) {
-               // System.out.println("SUCCESS!:" + file.getAbsolutePath());
+            if (file.getName().equals(filename)) {
                 found = file;
                 return file.getAbsolutePath();
             }
-            else if(file.isDirectory()) {
-               // System.out.println("DIRECTORY:" + file.getAbsolutePath());
-                findFilepathRecursively(file.listFiles(), filename);
+            else if(!file.getName().equals(filename) && file.isDirectory()) {
+
+                findFile(file.listFiles(), filename);
             }
         }
         return null;
--- src/test/java/fps/subskipper/util/FindFilePathTest.java
+++ src/test/java/fps/subskipper/util/FindFilePathTest.java
@@ -9,9 +9,14 @@ public class FindFilePathTest {
     @Test
         public void findFilepathRecursivelyTest (){
             FindFilePath findFile = new FindFilePath();
-            findFile.findFilepathRecursively(System.getProperty("user.dir"), "FindFilePathTest.java");
+            findFile.findFile(System.getProperty("user.dir"), "FindFilePathTest.java");
             assertEquals("D:\\002-git\\SubSkipper\\src\\test\\java\\fps\\subskipper\\util\\FindFilePathTest.java",
                     findFile.found.getAbsolutePath());
+
+        findFile.findFile("D:\\002-git\\SubSkipper", "util");
+        assertEquals("D:\\002-git\\SubSkipper\\target\\test-classes\\fps\\subskipper\\util",
+                findFile.found.getAbsolutePath());
+
     }
 }
 
