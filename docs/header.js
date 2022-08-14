7768bbe959b643d69dc2c10fd64e3d03dc37a942
FPS <fsocko@gmail.com>
2022-08-14 19:44:12 +0100

    pages - restore page.

---
+++ docs/header.js
@@ -0,0 +1,11 @@
+document.write('\
+<header>\
+    <div class="header screwpanel" id=headerDiv>\
+      <a href="https://fsocko.github.io/SubSkipper/" class="btn"><h1>SubSkipper</h1></a>\
+	  <h2>A series of tools for plotting torpedo solutions.</h2>\
+	  <a href="https://github.com/fsocko/SubSkipper" class="btn">View on GitHub</a> <br />\
+      <a href="https://github.com/fsocko/SubSkipper/zipball/master" class="btn">Download .zip</a> <br />\
+      <a href="https://github.com/fsocko/SubSkipper/tarball/master" class="btn">Download .tar.gz</a> <br />\
+	</div>\
+</header>\
+');
