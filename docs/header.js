a6e7107230c2323782253b83ade2d3b32a6432d6
fps <fsocko@gmail.com>
2023-02-05 17:52:09 +0000

    SS - Fix links, correct PDF campaign doc missing pages.

--- docs/header.js
+++ docs/header.js
@@ -1,11 +1,11 @@
 document.write('\
 <header>\
     <div class="header screwpanel" id=headerDiv>\
-      <a href="https://fsocko.github.io/SubSkipper/" class="btn"><h1>SubSkipper</h1></a>\
+      <a href="https://fsocko.github.io/subskipper/" class="btn"><h1>subskipper</h1></a>\
 	  <h2>A series of tools for plotting torpedo solutions.</h2>\
-	  <a href="https://github.com/fsocko/SubSkipper" class="btn">View on GitHub</a> <br />\
-      <a href="https://github.com/fsocko/SubSkipper/zipball/master" class="btn">Download .zip</a> <br />\
-      <a href="https://github.com/fsocko/SubSkipper/tarball/master" class="btn">Download .tar.gz</a> <br />\
+	  <a href="https://github.com/fsocko/subskipper" class="btn">View on GitHub</a> <br />\
+      <a href="https://github.com/fsocko/subskipper/zipball/master" class="btn">Download .zip</a> <br />\
+      <a href="https://github.com/fsocko/subskipper/tarball/master" class="btn">Download .tar.gz</a> <br />\
 	</div>\
 </header>\
 ');
